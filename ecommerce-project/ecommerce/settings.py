45f33e17e8867c4be0ebb14b7ff1b1ed2e9de51e
vitosv03 <vitosv03@gmail.com>
2021-11-21 14:05:00 +0200

    connect PostGreSQL database

--- ecommerce-project/ecommerce/settings.py
+++ ecommerce-project/ecommerce/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'PyPass-99',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
