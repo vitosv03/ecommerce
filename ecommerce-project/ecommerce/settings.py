c3f3532ed8a03e62285df88e2e0b1ce7bdc2204d
vitosv03 <vitosv03@gmail.com>
2021-11-21 21:25:38 +0200

    add static folder and registered static locations

--- ecommerce-project/ecommerce/settings.py
+++ ecommerce-project/ecommerce/settings.py
@@ -122,11 +122,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'ecommerce/static/'
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
