2bebfb622b60c8a9c6931b0e560ccffd5ac22d75
vitosv03 <vitosv03@gmail.com>
2021-11-22 18:16:26 +0200

    refactor: name 'about.html' to product.html

--- ecommerce-project/shop/urls.py
+++ ecommerce-project/shop/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('product/', views.about, name='product'),
+    path('product/', views.product, name='product'),
 ]
