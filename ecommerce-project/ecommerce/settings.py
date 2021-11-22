f063126f43a50b1b26b0121017000ccd292b3868
vitosv03 <vitosv03@gmail.com>
2021-11-22 23:07:18 +0200

    1. create model Category 2. create model Product 3. registered Category, Product into admin.py 4. connect/create folders for media storage in settings.py 5. registered media storage in urls.py

--- ecommerce-project/ecommerce/settings.py
+++ ecommerce-project/ecommerce/settings.py
@@ -137,3 +137,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/ecommerce_media/'
+
+MEDIA_ROOT = BASE_DIR / 'media/'
--- ecommerce-project/ecommerce/urls.py
+++ ecommerce-project/ecommerce/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
 ]
+# если DEBUG = True
+# обновляем файл shop/url.py только в режиме DEBUG = True
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
