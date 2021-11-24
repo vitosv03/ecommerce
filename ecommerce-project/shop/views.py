a59558ec69fc9fcc6a001928ff92e5c494ab30bd
vitosv03 <vitosv03@gmail.com>
2021-11-24 23:40:01 +0200

    add functional for buttons remove product from card and decrement by one product

--- ecommerce-project/shop/views.py
+++ ecommerce-project/shop/views.py
@@ -79,3 +79,23 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
                       counter=counter
                   )
                   )
+
+
+def cart_remove(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart_detail')
+
+
+def cart_remove_product(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart_detail')
