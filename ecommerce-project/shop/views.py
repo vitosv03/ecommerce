4987fffaf298e6eb2cc7596e2fd99586c47fad4b
vitosv03 <vitosv03@gmail.com>
2021-11-26 01:21:34 +0200

    create login and logout 1. create login page 2. add login and logout to navbar.html 3. create routes login and logout

--- ecommerce-project/shop/views.py
+++ ecommerce-project/shop/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, redirect
 
 from .forms import SignUpForm
 from .models import Category, Product, Cart, CartItem
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 
 
 # Create your views here.
@@ -116,3 +118,26 @@ def signUpView(request):
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+
+def loginView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return redirect('signup')
+
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def sign_outView(request):
+    logout(request)
+    return redirect('login')
